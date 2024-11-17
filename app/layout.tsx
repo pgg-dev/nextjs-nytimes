375e3254ee7b4ef72a9f2815a143127177fce174
mia <mia@mia-MacBookPro.local>
2024-11-18 03:00:37 +0900

    add navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navigation from "@/components/navigation";
+import Container from "@/components/container";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navigation />
+        <Container>{children}</Container>
       </body>
     </html>
   );
---
+++ components/navigation.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from "@/styles/navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <ul className={styles.container}>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+    </ul>
+  );
+}
